3af1c65981821dec2edc637a971a1a493f3f1ec0
GlinZachariah <glincool.zac@gmail.com>
2019-01-15 18:20:07 +0530

    added base functionlity

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-from PIL import ImageGrab
+from PIL import ImageGrab,ImageOps
 import pyautogui
 import time
+from numpy import *
 
 class Coordinates():
     restart=(1025,383)
-    dino =(850,390)
+    dino =(850,385)
 
 def restartGame() :
     pyautogui.click(Coordinates.restart)
@@ -15,6 +16,20 @@ def pressSpace():
     print("jump")
     pyautogui.keyUp('space')
     
-restartGame()    
-time.sleep(1)
-pressSpace()
+def imageGrab():
+    # box = ( Coordinates.dino[0]+7,Coordinates.dino[1]+7,Coordinates.dino[0]+30,Coordinates.dino[1]+35)
+    box=(857,381,880,415)
+    image =ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a= array(grayImage.getcolors())
+    print (a.sum())
+    return a.sum()
+
+def main() :
+    restartGame()    
+
+    while True:
+        if(imageGrab() > 1029):
+            pressSpace()
+
+main()
