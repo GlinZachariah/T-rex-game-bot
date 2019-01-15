620172d6dcdf99871e58644627baae85a9fc4f34
GlinZachariah <glincool.zac@gmail.com>
2019-01-15 17:49:59 +0530

    add jump function

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
 from PIL import ImageGrab
 import pyautogui
+import time
 
 class Coordinates():
     restart=(1025,383)
+    dino =(850,390)
 
 def restartGame() :
     pyautogui.click(Coordinates.restart)
 
+def pressSpace():
+    pyautogui.keyDown('space')
+    time.sleep(0.05)
+    print("jump")
+    pyautogui.keyUp('space')
+    
 restartGame()    
+time.sleep(1)
+pressSpace()
